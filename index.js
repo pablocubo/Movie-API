8b465c0ee8ed12c6690b0d6b38991eddf8b4fe04
Pablo Lara Roloff <pablo.lara.roloff@gmail.com>
2024-01-22 13:00:14 +0100

    testing CORS

--- index.js
+++ index.js
@@ -11,11 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS
 const cors = require('cors');
-
 let allowedOrigins = ['http://localhost:1234', 'http://testsite.com', 'http://testsite2.com'];
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || allowedOrigins,
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isnt found on the list of allowed origins
+        let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+        return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 // importing auth.js file
